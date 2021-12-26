675180f72bb2aef3de471bf27df5379070d478aa
sb920502 <bhowmik920502@gmail.com>
2021-12-26 12:41:04 +0600

    first commit

---
+++ src/test/java/com/SpringApp02/SpringApp02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringApp02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApp02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
